ca2c1a0f59dcb84a70ae3de5c862cbf056f4ed9d
Paul Scarrone <ninjapanzer@gmail.com>
2016-01-23 15:07:39 -0500

    A simple program talking about style and streams with operations

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int awesome_number = 0;
+    awesome_number = awesome_number + 1;
+    string name = "";
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << name << endl;
+    return 0;
+}
